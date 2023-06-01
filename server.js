95d1a588190ca05ee4daa2edcaa7177625a9ddd8
Anastasiia Herusova <nastia.herusova@gmail.com>
2023-06-01 13:50:42 +0300

    dotenv

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose");
 
-
-  // "mongodb+srv://Anastasiia:JUdewhNGYLnHzRYF@cluster0.ffsyvzp.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
 const app = require("./app");
 
-const {DB_HOST} = process.env;
+const { DB_HOST } = process.env;
 
-mongoose.connect(DB_HOST)
+mongoose
+  .connect(DB_HOST)
   .then(() => {
     app.listen(3000);
   })
-  .catch(error => {
+  .catch((error) => {
     console.log(error.message);
     process.exit(1);
   });
