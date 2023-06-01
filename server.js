061fdd4c655f01f75eca6db8cab3a0ac6f6a0758
Anastasiia Herusova <nastia.herusova@gmail.com>
2023-06-01 14:12:07 +0300

    delete process.env

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 
-const app = require("./app");
+const DB_HOST =
+  "mongodb+srv://Anastasiia:JUdewhNGYLnHzRYF@cluster0.ffsyvzp.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
-const { DB_HOST } = process.env;
+const app = require("./app");
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(3000);
   })
-  .catch((error) => {
-    console.log(error.message);
+  .catch(error => {
+    console.log(error);
     process.exit(1);
   });
